b9592d0d472cf11f52e35a9578d8fbf1752fb662
sorkihg <sorkihg@gmail.com>
2022-05-09 21:32:27 +0900

    CREATE : C.R.U.D 2_owner과제 초기세팅

--- owners/models.py
+++ owners/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Owner(models.Model):
+    owner_name = models.CharField(max_length=45)
+    owner_email = models.CharField(max_length=300)
+    owner_age = models.IntegerField()
+
+    class Meta:
+        db_table = "owner"
+
+
+class Dogs(models.Model):
+    dog_name = models.CharField(max_length=45)
+    dog_age = models.IntegerField()
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "dogs"
